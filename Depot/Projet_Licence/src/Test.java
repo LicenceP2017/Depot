2447eee2dbd6c94bac47c25f0f38a80277464f28
Quentin <quentin.perdrix@hotmail.fr>
2018-02-23 12:07:58 +0100

    Ajout form ajouter et instanciation de la fenetre ajout dans le main Test

--- Depot/Projet_Licence/src/Test.java
+++ Depot/Projet_Licence/src/Test.java
@@ -1,20 +1,25 @@
-
+import testPro.ajouter;
 
 public class Test {
 
-public Test() {
-// TODO Auto-generated constructor stub
+	public Test() {
+		// TODO Auto-generated constructor stub
 }
 
-/**
- * @param args
- */
-public static void main(String[] args) {
-// TODO Auto-generated method stub
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-InterfaceConnexion  ic = new InterfaceConnexion();
-ic.setTitle("Logiciel de Formation");
-ic.setVisible(true);
-}
+		InterfaceConnexion  ic = new InterfaceConnexion();
+		ic.setTitle("Logiciel de Formation");
+		ic.setVisible(true);
+		
+		// TODO Instancier une JFrame
+		ajouter fenetreAjout = new ajouter();
+		// TODO Afficher la JFrame
+		fenetreAjout.setVisible(true);
+	}
 
 }
--- Depot/Projet_Licence/src/ajouter.java
+++ Depot/Projet_Licence/src/ajouter.java
@@ -1,6 +1,5 @@
 
-import javax.swing.JFrame;
-import javax.awt.BorderLayout;
+import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class ajouter extends JFrame {
@@ -9,28 +8,37 @@ public class ajouter extends JFrame {
 	private JButton bouton = new JButton("Ajouter");
 	private JPanel conteneur = new JPanel();
 	private JTextField tf1 = new JTextField(10);
+	private JComboBox<String> combo = new JComboBox<String>();
 	
+	// TODO Constructeur de la fenêtre
 	
-	
-	
-	// constructeur 
-	public Fenetre(){
+	public ajouter(){
 		
-		setTitle("Ajout d'une vidéo");
-		setSize(300, 400);
+		// TODO Modifier le titre de la fenêtre
+		setTitle("Ajouter une vidéo");
+		// TODO Modifier la taille
+		setSize(400, 300);
+		// TODO Taille non modifiable par l'utilisateur
 		setResizable(false);
-		setLocationRelativeTo(null);
+		// TODO Un click sur croix entraine fermeture de la fenêtre
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// TODO Centrer la fenêtre par rapport à l'écran de l'odinateur
+		setLocationRelativeTo(null);
 		
-		//BorderLayout
-		conteneur.setLayout(BorderLayout());
-		
-		//remplissage du conteneur
-		conteneur.add(bouton,BorderLayout.CENTER);
+		// ajout bouton sur le conteneur
+		conteneur.add(bouton);
+		// ajout label
 		conteneur.add(lb1);
+		//ajout textField
 		conteneur.add(tf1);
+		//ajout éléments dans la combo
+		combo.addItem("Thème1");
+		combo.addItem("Thème2");
+		combo.addItem("Thème3");
+		// affichage de la combo
+		conteneur .add(combo);
+	
+		
 		setContentPane(conteneur);
-		pack();
 	}
-
 }
